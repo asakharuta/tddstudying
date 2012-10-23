01ef7fe64711f9812d23185db4a8074c4ee612f3
Andriy Sakharuta <saint.sakharuta@gmail.com>
2012-10-23 19:07:28 +0300

    test

--- AuctionSniper/src/ua/com/asakharuta/auctionsniper/common/Constants.java
+++ AuctionSniper/src/ua/com/asakharuta/auctionsniper/common/Constants.java
@@ -5,5 +5,6 @@ public class Constants
 	public static final String AUCTION_RESOURCE = "Auction";
 	public static final String ITEM_ID_AS_LOGIN = "auction-%s";
 	public static final String AUCTION_PASSWORD = "auction";
+	
 	public static final String AUCTION_ID_FORMAT = ITEM_ID_AS_LOGIN + "@%s/" + AUCTION_RESOURCE;
 }
