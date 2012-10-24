50a961c8c26b769000178c364c3fe527de5cb030
Andriy Sakharuta <saint.sakharuta@gmail.com>
2012-10-24 16:29:08 +0300

    AuctionMessageTranslator and AuctionEventListener are introduced

---
+++ AuctionSniper/src/ua/com/asakharuta/auctionsniper/AuctionEventListener.java
@@ -0,0 +1,6 @@
+package ua.com.asakharuta.auctionsniper;
+
+public interface AuctionEventListener
+{
+	void auctionClosed();
+}
---
+++ AuctionSniper/test/ua/com/asakharuta/unit/AuctionMessageTranslatorTest.java
@@ -0,0 +1,41 @@
+package ua.com.asakharuta.unit;
+
+import static org.junit.Assert.*;
+
+import org.hamcrest.Description;
+import org.jivesoftware.smack.Chat;
+import org.jivesoftware.smack.MessageListener;
+import org.jivesoftware.smack.packet.Message;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.api.Expectation;
+import org.jmock.api.Invocation;
+import org.junit.Test;
+
+import ua.com.asakharuta.auctionsniper.AuctionEventListener;
+import ua.com.asakharuta.auctionsniper.AuctionMessageTranslator;
+
+public class AuctionMessageTranslatorTest
+{
+	private final Mockery context = new Mockery();
+	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
+	
+	private static final Chat UNUSED_NULL_CHAT = null;
+	private final AuctionMessageTranslator translator = new AuctionMessageTranslator(listener);
+	
+	@Test
+	public void notifiesAuctionClosedWhenCloseMessageReceived()
+	{
+		context.checking(new Expectations(){{
+			oneOf(listener).auctionClosed();
+		}}
+		);
+	
+		Message message = new Message();
+		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
+		translator.processMessage(UNUSED_NULL_CHAT, message);
+		
+		context.assertIsSatisfied();
+	}
+
+}
